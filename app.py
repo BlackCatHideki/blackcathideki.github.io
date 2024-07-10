21dffd69befd6fcba3b7c7f1c676ca3ab9977f7e
Hideki <155396465+techcatHideki@users.noreply.github.com>
2024-07-09 20:12:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Personal and professional information to be displayed on the homepage
-    name = 'Carlos Jurado'
-    tags = 'Bioengineering | Brain modulation | Artificial Intelligence'
-    description = "I am a dedicated professional with a comprehensive background in bioengineering and artificial intelligence. Currently, I serve as a Clinical Specialist at Medtronic, where I utilize my expertise to address complex challenges in high-stress environments. My career journey has been enriched by various roles, including those of Laboratory Assistant and Research Student at the University of Antioquia."
+    name = 'Hideki'
+    tags = 'Gluttony kitty'
+    description = "🐱 Chubby furry cat ♥️ Eating, Anime & Linux 💻 Bioengineering & AI 🎮 Wakfu 🇬🇧 🇪🇸 Speaker"
     links = [
         # Social and professional links to be included on the homepage
-        {'name': 'GitHub', 'url': 'https://github.com/EngJurado', 'icon': 'fa-github'},
-        {'name': 'LinkedIn', 'url': 'https://www.linkedin.com/in/engjurado/', 'icon': 'fa-linkedin'},
-        {'name': 'Telegram', 'url': 'https://telegram.me/engjurado', 'icon': 'fa-telegram'},
-        {'name': 'Twitter', 'url': 'https://twitter.com/EngJurado', 'icon': 'fa-twitter'}
+        {'name': 'BlueSky', 'url': 'https://bsky.app/profile/techcathideki.bsky.social', 'icon': 'fa-bluesky'},
+        {'name': 'Discord', 'url': 'https://discordapp.com/users/techcathideki', 'icon': 'fa-discord'},
+        {'name': 'Facebook', 'url': 'https://www.facebook.com/techcatHideki/', 'icon': 'fa-facebook'},
+        {'name': 'GitHub', 'url': 'https://github.com/techcathideki', 'icon': 'fa-github'},
+        {'name': 'Line', 'url': 'https://line.me/ti/p/iTlebHiBCU', 'icon': 'fa-line'},
+        {'name': 'pixiv', 'url': 'https://www.pixiv.net/en/users/103988843', 'icon': 'fa-pixiv'},
+        {'name': 'Steam', 'url': 'https://steamcommunity.com/id/techcatHideki/', 'icon': 'fa-steam'},
+        {'name': 'Telegram', 'url': 'https://telegram.me/techcatHideki', 'icon': 'fa-telegram'},
+        {'name': 'Twitter', 'url': 'https://twitter.com/techcatHideki', 'icon': 'fa-x-twitter'}
     ]
     # Render the homepage template with the provided information
     return render_template('index.html', name=name, tags=tags, description=description, links=links)
